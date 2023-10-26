cf520fcb7ae3912bc5df0e1c2604520eb0bd02ca
vinayreddygujjula <vinayreddyg02@gmail.com>
2023-10-25 21:52:37 -0500

    calc sum

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+var sum = 0;
+var more = 0;
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  more += 1;
+  sum += more;
+  res.send(`Sum is: ${sum}`);
 });
-
+ 
 module.exports = router;
