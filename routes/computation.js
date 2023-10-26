b1252406fa3336c637cd175523752978b5b3638e
vinayreddygujjula <vinayreddyg02@gmail.com>
2023-10-25 22:32:27 -0500

    bonus - query arg

--- routes/computation.js
+++ routes/computation.js
@@ -2,13 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    const randomValue = Math.floor(Math.random() * (20)) + 1;
+    var queryargCheck = req.query.queryargCheck;
+    var randomValue;
+    if(queryargCheck == undefined){
+        randomValue = Math.random();
+    }
+    else{
+        randomValue = queryargCheck;
+    }
     const abs = Math.abs(randomValue);
     const acos = Math.acos(randomValue);
     const sin = Math.sin(randomValue);
     const resultString =
-        `Math.abs applied to ${randomValue} is ${abs}       
-         Math.acos applied to ${randomValue} is ${acos}     
+        `Math.abs applied to ${randomValue} is ${abs}   <br>    
+         Math.acos applied to ${randomValue} is ${acos}     <br>
          Math.acos applied to ${randomValue} is ${sin}`;
     res.send(resultString);
 });
